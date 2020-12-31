5912d13bb4cd88eafd6923b94d43a48b778372a4
THETIGERSEYES <samias120@yahoo.fr>
2020-12-31 12:13:23 +0100

    new test app.integration.spec.js

---
+++ test/app.integration.spec.js
@@ -0,0 +1,17 @@
+const request  = require('supertest');
+const app = require('../app.js');
+
+describe('Test routes', () => {
+  it('GET / sends "Hello World" as json', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        const expected = { message: 'Hello World!'};
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+});
+
